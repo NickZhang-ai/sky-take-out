ec0c01621e13947539bdcfe2ba2d0375b96b0db2
Nick <nickzhangjy@126.com>
2025-03-01 03:48:59 -0500

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -5,6 +5,8 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface DishService {
 
@@ -20,4 +22,11 @@ public interface DishService {
      * @return
      */
     PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     * @return
+     */
+    void deleteBatch(List<Long> ids);
 }
